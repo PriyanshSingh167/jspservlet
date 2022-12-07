c0135bcfa2b4ea91c6663da45d1a9ebb3dd69c4d
priyanshsingh <priyansh@singh.com>
2022-12-07 11:44:45 +0530

    Calling servlet from a servlet using Request Dispatcher

--- src/com/priyansh/SqServlet.java
+++ src/com/priyansh/SqServlet.java
@@ -3,7 +3,6 @@ package com.priyansh;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AddNumbers
+ * Servlet implementation class SqServlet
  */
-@WebServlet("/AddNumbers")
-public class AddNumbers extends HttpServlet {
+@WebServlet("/SqServlet")
+public class SqServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -23,19 +22,11 @@ public class AddNumbers extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		int i = Integer.parseInt(request.getParameter("num1"));
-		int j = Integer.parseInt(request.getParameter("num2"));
-		
-		int k = i + j;
-		request.setAttribute("k", k);
-		
-		// There are tow methods to call a servlet from another servlet: Request dispatcher And Redirect
-		
-		RequestDispatcher rd = request.getRequestDispatcher("sq");
-		rd.forward(request, response);
-		
+		int k = (int) request.getAttribute("k");
+		k *= k;
+		PrintWriter out = response.getWriter();
+		out.println("The Square of two provided numbers is " + k);
 		
 	}
-	
-	
+
 }
